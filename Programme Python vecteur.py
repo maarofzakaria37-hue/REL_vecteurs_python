77f23830c56dcf2d7b83b82e115ef415a4438380
maarofzakaria37-hue <maarofzakaria37@gmail.com>
2026-03-28 22:02:45 +0100

    Update Programme Python vecteur.py

--- Programme Python vecteur.py
+++ Programme Python vecteur.py
@@ -11,19 +11,19 @@ N = len(x)
 dt = 0.04
 
 # Calcul des vitesses
-vx = (x[1:] - x[:-1]) / dt
-vy = (y[1:] - y[:-1]) / dt
+vx = (x[1:]-x[:-1]) / dt
+vy = (y[1:]-y[:-1]) / dt
 
 # Tracé des points
 plt.plot(x, y, "bo")
 
 # Tracé des vecteurs vitesse (on utilise x[:-1] car vx a N-1 valeurs)
-plt.quiver(x[:-1], y[:-1], vx, vy,
-           angles="xy", scale_units="xy", scale=40, color="red")
+plt.quiver(x[:-1], y[:-1],vx,vy,
+angles="xy",scale_units="xy",scale=40,color="red")
 
 # Configuration du graphique
-plt.xlabel("x (m)")
-plt.ylabel("y (m)")
+plt.xlabel("x(m)")
+plt.ylabel("y(m)")
 plt.title("Chronophotographie du mouvement")
 plt.axis("equal")
 plt.grid()
