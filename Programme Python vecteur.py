a2c5e05c9f802a1847283d0e5f9c8fd3716ca6e4
maarofzakaria37-hue <maarofzakaria37@gmail.com>
2026-03-28 22:01:27 +0100

    Create Programme Python vecteur.py

---
+++ Programme Python vecteur.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Positions du mobile
+x = np.array([])
+
+y = np.array([])  #
+
+
+N = len(x)
+dt = 0.04
+
+# Calcul des vitesses
+vx = (x[1:] - x[:-1]) / dt
+vy = (y[1:] - y[:-1]) / dt
+
+# Tracé des points
+plt.plot(x, y, "bo")
+
+# Tracé des vecteurs vitesse (on utilise x[:-1] car vx a N-1 valeurs)
+plt.quiver(x[:-1], y[:-1], vx, vy,
+           angles="xy", scale_units="xy", scale=40, color="red")
+
+# Configuration du graphique
+plt.xlabel("x (m)")
+plt.ylabel("y (m)")
+plt.title("Chronophotographie du mouvement")
+plt.axis("equal")
+plt.grid()
+
+plt.show()
